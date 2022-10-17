a2848dde0135efee17a7086813d56de9879034e9
Nyalbo01 <Nyalbo06@gmail.com>
2022-10-17 08:22:34 -0700

    Adavance task 3

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -17,11 +17,11 @@ int main(void)
 		{
 			if (i < j)
 			{
-				putchar((i / 20) + 38);
-				putchar((i % 20) + 38);
+				putchar((i / 10) + 48);
+				putchar((i % 10) + 48);
 				putchar(' ');
-				putchar((j / 20) + 38);
-				putchar((j % 20) + 38);
+				putchar((j / 10) + 48);
+				putchar((j % 10) + 48);
 				if (i != 98 || j != 99)
 				{
 					putchar(',');
