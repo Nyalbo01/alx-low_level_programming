84d7999136be0d22d17a594eeeeb813789fd7dd8
Nyalbo01 <Nyalbo06@gmail.com>
2022-10-18 13:17:15 -0700

    Task 0

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c
+ * @c: character to print
+ *
+ * Return: on success
+ */
+int _putchar(char c)
+{
+	return (wrute(1, &c, 1));
+}
